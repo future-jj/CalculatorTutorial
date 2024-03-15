54e16ad98cd6374b2ca1a1d4939afd8bd2bd07e2
future_zjj <1805862661@qq.com>
2024-03-15 23:17:46 +0800

    添加项目文件。

---
+++ CalculatorTutorial/CalculatorTutorial.cpp
@@ -0,0 +1,15 @@
+﻿// CalculatorTutorial.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+        << endl;
+    return 0;
+}
+
